d4f7da2230e8aa97480806eb43ee504a91cfe440
codeEmpress1 <alimot.akinbode@canonical.com>
2025-05-06 18:43:15 +0300

    update nav

--- static/js/src/store/components/Banner/Banner.tsx
+++ static/js/src/store/components/Banner/Banner.tsx
@@ -1,47 +1,16 @@
-import { RefObject } from "react";
-import { useSearchParams } from "react-router-dom";
 import { Strip, Row, Col } from "@canonical/react-components";
 
-type Props = {
-  searchRef: RefObject<HTMLInputElement>;
-  searchSummaryRef: RefObject<HTMLDivElement>;
-};
-
-function Banner({ searchRef }: Props) {
-  const [searchParams, setSearchParams] = useSearchParams();
+function Banner() {
 
   return (
     <Strip type="dark">
       <Row>
-        <Col size={6} className="col-start-large-4">
-          <h1 className="p-heading--2">The Rocks Collection</h1>
-          <form className="p-search-box" action="/all-search">
-            <label className="u-off-screen" htmlFor="search">
-              Search Rocks
-            </label>
-            <input
-              type="search"
-              id="search"
-              className="p-search-box__input"
-              name="q"
-              placeholder="Search Rocks"
-              defaultValue={searchParams.get("q") || ""}
-              ref={searchRef}
-            />
-            <button
-              type="reset"
-              className="p-search-box__reset"
-              onClick={() => {
-                searchParams.delete("q");
-                setSearchParams(searchParams);
-              }}
-            >
-              <i className="p-icon--close">Close</i>
-            </button>
-            <button type="submit" className="p-search-box__button">
-              <i className="p-icon--search">Search</i>
-            </button>
-          </form>
+        <Col size={6}>
+          <h1 className="p-heading--2">
+            The Ubuntu-based
+            <br/>
+            container images store
+          </h1>
         </Col>
       </Row>
     </Strip>
