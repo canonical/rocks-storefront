1fde98956ece7ac498866ad4b0fba3d52b11735a
Alvaro Mateo <alvaromateo9@gmail.com>
2025-06-13 14:34:41 +0200

    fix: check-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,4 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require("eslint/config");
 
 const tsParser = require("@typescript-eslint/parser");
 const jest = require("eslint-plugin-jest");
@@ -10,63 +8,63 @@ const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const globals = require("globals");
 const js = require("@eslint/js");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
 
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            ...jest.environments.globals.globals,
-            ...globals.jest,
-            ga: "readonly",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...jest.environments.globals.globals,
+        ...globals.jest,
+        ga: "readonly",
+      },
+
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-
-        sourceType: "module",
-
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-        },
+      },
     },
 
     plugins: {
-        jest,
-        react,
-        "@typescript-eslint": typescriptEslint,
+      jest,
+      react,
+      "@typescript-eslint": typescriptEslint,
     },
 
     extends: compat.extends(
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended"
     ),
 
     rules: {
-        "linebreak-style": ["error", "unix"],
-        semi: ["error", "always"],
-        "object-curly-spacing": ["error", "always"],
-        "react/react-in-jsx-scope": "off",
-        "react/display-name": "off",
-        "react/no-unescaped-entities": "off",
-        "no-undef": "off",
+      "linebreak-style": ["error", "unix"],
+      semi: ["error", "always"],
+      "object-curly-spacing": ["error", "always"],
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "react/no-unescaped-entities": "off",
+      "no-undef": "off",
     },
 
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: "detect",
+      },
     },
-}]);
+  },
+]);
