645e3ba804ad5a484506f50b24f4b95c34012501
codeEmpress1 <alimot.akinbode@canonical.com>
2025-06-19 10:53:30 +0300

    chore: remove unused blueprint

--- webapp/config.py
+++ webapp/config.py
@@ -3,15 +3,4 @@ import os
 APP_NAME = "rockstore"
 DETAILS_VIEW_REGEX = "[A-Za-z0-9-]*[A-Za-z][A-Za-z0-9-]*"
 
-SEARCH_FIELDS = [
-    "result.categories",
-    "result.summary",
-    "result.media",
-    "result.title",
-    "result.publisher.display-name",
-    "default-release.revision.revision",
-    "default-release.channel",
-    "result.deployable-on",
-]
-
 SENTRY_DSN = os.getenv("SENTRY_DSN", "").strip()
