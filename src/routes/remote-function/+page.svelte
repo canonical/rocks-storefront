f1f615ec11c9932690d70878dcd768c2a70463a9
Eduard M <4310497+edisile@users.noreply.github.com>
2026-07-02 17:00:45 +0200

    feat: API client as remote function

---
+++ src/routes/remote-function/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { getRocks } from "$lib/remote/api.remote";
+
+  const rocks = (await getRocks({})).results;
+</script>
+
+<h1>Rocks store</h1>
+<h2>
+    Result: {rocks.length} rocks
+</h2>
+<ul>
+    {#each rocks as rock}
+        <li>{rock.name}</li>
+    {/each}
+</ul>
