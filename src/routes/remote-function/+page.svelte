bd77780291706e08689330375fedf0498b5d4c0f
Eduard M <4310497+edisile@users.noreply.github.com>
2026-07-03 14:16:14 +0100

    fix: more correct remote function usage

--- src/routes/remote-function/+page.svelte
+++ src/routes/remote-function/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
   import { getRocks } from "$lib/remote/api.remote";
-
-  const rocks = (await getRocks({})).results;
+  import { type RockFindResponse } from "$lib/server/api/types";
 </script>
 
 <h1>Rocks store</h1>
-<h2>
-    Result: {rocks.length} rocks
-</h2>
-<ul>
-    {#each rocks as rock (rock.name)}
-        <li>{rock.name}</li>
-    {/each}
-</ul>
+{@render rocksList(await getRocks({}))}
+
+{#snippet rocksList(rocks: RockFindResponse)}
+    {@const results = rocks.results}
+    <h2>
+        Result: {results.length} rocks
+    </h2>
+    <ul>
+        {#each results as rock (rock.name)}
+            <li>{rock.name}</li>
+        {/each}
+    </ul>
+{/snippet}
