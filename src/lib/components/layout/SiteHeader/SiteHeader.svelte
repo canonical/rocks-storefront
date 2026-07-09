a418eda5ec5439ab9f39f1bf29dc33bc852b6845
Eduard M <4310497+edisile@users.noreply.github.com>
2026-07-09 12:38:57 +0200

    fix: header logo in dark mode

--- src/lib/components/layout/SiteHeader/SiteHeader.svelte
+++ src/lib/components/layout/SiteHeader/SiteHeader.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import logo from "$lib/assets/rockstore-logo.svg";
+  import logo from "$lib/assets/rockstore-logo.svg?raw";
   import "./styles.css";
 </script>
 
 <header class="ds site-header">
   <div class="app-container">
     <a href="/" aria-label="Rockstore home">
-      <img src={logo} alt="Rockstore" width="122" height="37" />
+      {@html logo}
     </a>
   </div>
 </header>
