516f2c23e618c926bc31bcbec2d0a9c580803c63
Eduard M <4310497+edisile@users.noreply.github.com>
2026-07-15 16:11:10 +0000

    feat: add search by categories

--- src/lib/components/ui/SearchForm/SearchForm.svelte
+++ src/lib/components/ui/SearchForm/SearchForm.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-  import { SearchBox } from "@canonical/svelte-ds-app-launchpad";
+  import { Checkbox, SearchBox } from "@canonical/svelte-ds-app-launchpad";
   import { SpinnerIcon } from "@canonical/svelte-icons";
   import "./styles.css";
 
   export type Props = {
     name: string;
-    value?: string | null;
+    value?: string;
     loading?: boolean;
   };
 
   let { name, value = $bindable(), loading }: Props = $props();
 </script>
 
-<form class="ds search-form" data-sveltekit-keepfocus>
+<form id="search-form" class="ds search-form" data-sveltekit-keepfocus>
     <label class="visually-hidden" for={name}>Search rocks</label>
     <SearchBox
         id={name}
         {name}
-        bind:value={value as string}
+        bind:value
         placeholder="Search for a rock"
         aria-label="Search rocks"
     >
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
-  import { queryParameters } from "sveltekit-search-params";
+  import { Checkbox } from "@canonical/svelte-ds-app-launchpad";
+  import { array, optional, string } from "valibot";
   import { RockList } from "$lib/components/rock/RockList";
   import { Heading } from "$lib/components/ui/Heading";
   import SearchForm from "$lib/components/ui/SearchForm/SearchForm.svelte";
   import { SmallCaps } from "$lib/components/ui/SmallCaps";
   import { getRocks } from "$lib/remote/api.remote";
   import debounced from "$lib/utils/debounced.svelte";
+  import searchParams from "$lib/utils/searchParams.svelte";
 
-  const params = queryParameters({ q: true });
-  const query = debounced(() => params.q, 200);
-  const rocksRequest = $derived(getRocks({ query: query() }));
+  const CATEGORIES = [
+    "Featured",
+    "Data",
+    "Identity",
+    "Networks",
+    "Observability",
+    "OS/Bases",
+    "Toolchains/Languages",
+    "Web",
+  ] as const;
+
+  const params = searchParams({
+    q: optional(string(), ""),
+    category: optional(array(string()), []),
+  });
+
+  const rocksRequest = $derived.by(() => {
+    const paramsDebounced = debounced(() => params, 200);
+    return getRocks({
+      query: paramsDebounced().q,
+      categories: paramsDebounced().category,
+    });
+  });
 </script>
 
 <svelte:head>
-    {#if query()}
+    {#if params.q}
         <title>
-            Search results for "{query()}" · Rock Store
+            Search results for "{params.q}" · Rock Store
         </title>
     {:else}
         <title>
@@ -31,7 +53,16 @@
         <aside style="grid-column: span 3">
             <Heading level={2}><SmallCaps>Categories</SmallCaps></Heading>
 
-            TODO...
+            <!-- TODO: change this in some way to move the form logic into the SearchForm component -->
+            <fieldset class="ds checkbox-group">
+                {#each CATEGORIES as category}
+                    {@const lower = category.toLowerCase() }
+                    <label class="ds checkbox-label" for="checkbox-{lower}">
+                        <Checkbox id="checkbox-{lower}" form="search-form" name="category" bind:group={params.category} value={category}/>
+                        {category}
+                    </label>
+                {/each}
+            </fieldset>
         </aside>
 
         <section style="grid-column: span 9">
@@ -45,5 +76,23 @@
 <style>
     .app-container {
         margin-block: var(--dimension-500);
+    }
+
+    .ds.checkbox-group {
+      --x-gap: var(--space-200);
+      --y-gap: var(--space-100);
+
+      padding: 0;
+      border: none;
+      display: grid;
+      row-gap: var(--y-gap);
+
+      label {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        column-gap: var(--x-gap);
+        align-items: center;
+        cursor: pointer;
+      }
     }
 </style>
